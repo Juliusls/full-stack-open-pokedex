84dec7814c8316490556760076bacca66ed742a0
Julius <julius.lubys@gmail.com>
2021-01-14 14:36:26 +0200

    Adding error to periodic check

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.get('/version', (req, res) => {
   res.send('34')
 })
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
